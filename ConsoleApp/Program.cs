c0a898f488168653ffb2de82efefbb610e94c440
Arsenii Petrov <arseniiptrv@gmail.com>
2019-03-25 18:50:58 +0200

    feature(name): description

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
