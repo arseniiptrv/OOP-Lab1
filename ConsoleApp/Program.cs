20016458a265d91715ff600f81509819c5e7df05
Arsenii Petrov <arseniiptrv@gmail.com>
2019-04-03 19:43:59 +0300

    chore(Main, discriminant): изменил код вызова метода discriminant в Main. Добавил комментарии в discriminant

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,88 +13,95 @@ namespace ConsoleApp
             
             //Console.OutputEncoding = Encoding.Unicode;
             //Console.InputEncoding = Encoding.Unicode;
+           
             
             
-            // ООП. Лабораторна робота 2
-            // Завдання з рівняння №2
-            /*Console.WriteLine("Введіть дійсне число x: ");
-            double x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(x.GetType().ToString());
-            if (x.GetType().ToString() != System.Double)
+            
+            
+            
+            
+            // Some test task
+            /*int[] arr = new int[] {1, 3, 5, 7, 9};
+            arr[2] = 15;
+            arr[5] = 25;
+            foreach (int q in arr)
             {
-                Console.WriteLine("Помилка введення значення x. Будь ласка, повторіть введення значення ще раз: ");
-                x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(q);
             }
+
+            for (int e = 0; e < arr.Length; e++)
+            {
+                Console.WriteLine(arr[e]);
+            }*/
             
-                        
-            Console.WriteLine("Введіть дійсне число y: ");
-            double y = Convert.ToDouble(Console.ReadLine());           
-            Console.WriteLine("Введіть дійсне число z: ");
-            double z = Convert.ToDouble(Console.ReadLine());            
-            Console.WriteLine(expr2(x, y, z));*/
+            
+            
+            
+            
             
             
             
             // ООП. Лабораторна робота 2
             // Завдання №2: Виклик методу discriminant для обчислення коренів квадратного рівняння
-            /*int x1;
-            int x2;
-            int D;
 
-     
-            Console.WriteLine("Введіть ціле значення a: ");
-            int a = Convert.ToInt32(Console.ReadKey());
-            while (a.GetType().ToString() != System.Int32)
-            {
-                Console.WriteLine("Ви ввели помилкове значення a. Будь ласка, спробуйте ще раз: ");
-                a = Convert.ToInt32(Console.ReadKey());
-            }
-            
-            
-            Console.WriteLine("Введіть ціле значення b: ");
-            int b = Convert.ToInt32(Console.ReadKey());
-            while (b.GetType().ToString() != System.Int32)
-            {
-                Console.WriteLine("Ви ввели помилкове значення b. Будь ласка, спробуйте ще раз: ");
-                b = Convert.ToInt32(Console.ReadKey());
-            }
-            
-            Console.WriteLine("Введіть ціле значення c: ");
-            int c = Convert.ToInt32(Console.ReadKey());
-            while (c.GetType().ToString() != System.Int32)
-            {
-                Console.WriteLine("Ви ввели помилкове значення c. Будь ласка, спробуйте ще раз: ");
-                c = Convert.ToInt32(Console.ReadKey());
-            }*/
-
-
+            // Оголошую змінні х1, х2 та D, у які буде передано значення квадратних коренів
+            // та дискримінанта після виклику метода discriminant
             int x1;
             int x2;
             int D;
-            discriminant(2, 6, 4, out x1, out x2, out D);
-            //discriminant(3, 5, 3, out x1, out x2, out D);
-            //discriminant(3, 6, 3, out x1, out x2, out D);
+            
+            // Запрошую користувача ввести ціле значення а. Є перевірка на коректність введення
+            Console.WriteLine("Введіть ціле значення a: ");
+            var inputA = Console.ReadLine();
+            int a;
+            while (!int.TryParse(inputA, out a))
+            {
+                Console.WriteLine("Ви ввели не число. Будь ласка, спробуйте ще раз: ");
+                inputA = Console.ReadLine();
+            }
+            Console.WriteLine("значення a: " + a);
+            Console.WriteLine("тип a: " + a.GetType().ToString());
+          
+            
+            // Запрошую користувача ввести ціле значення b. Є перевірка на коректність введення
+            Console.WriteLine("Введіть ціле значення b: ");
+            var inputB = Console.ReadLine();
+            int b;
+            while (!int.TryParse(inputB, out b))
+            {
+                Console.WriteLine("Ви ввели не число. Будь ласка, спробуйте ще раз: ");
+                inputB = Console.ReadLine();
+            }
+            Console.WriteLine("значення b: " + b);
+            Console.WriteLine("тип b: " + b.GetType().ToString());
+
+            
+            // Запрошую користувача ввести ціле значення c. Є перевірка на коректність введення
+            Console.WriteLine("Введіть ціле значення c: ");
+            var inputC = Console.ReadLine();
+            int c;
+            while (!int.TryParse(inputC, out c))
+            {
+                Console.WriteLine("Ви ввели не число. Будь ласка, спробуйте ще раз: ");
+                inputC = Console.ReadLine();
+            }
+            Console.WriteLine("значення c: " + c);
+            Console.WriteLine("тип c: " + c.GetType().ToString());
+
+            
+            
+            // Викликаю метод дискримінант
+            discriminant(a, b, c, out x1, out x2, out D);
         }
 
         
-        
-        /*static double expr2(double x, double y, double z)
-        {
-            double first = Math.Pow(3 + (x - y), 1.0 / 3.0);
-            double second = (x * x) + (z * z * z) + 4;
-            double third = Math.Tan(z);
-
-            //double res = first / second;
-            return first / second - third;
-        }*/
-
-        
+       
 
         static void discriminant(int a, int b, int c, out int x1, out int x2, out int D)
         {
-            D = b * b - 4 * a * c;          // змінна D - містить значення дискримінанту
-            x1 = 0;                                                         // змінна х1 - містить значення квадратного кореня №1
-            x2 = 0;                                                         // змінна х2 - містить значення квадратного кореня №2
+            D = b * b - 4 * a * c;                           // змінна D - містить значення дискримінанту
+            x1 = 0;                                          // змінна х1 - містить значення квадратного кореня №1
+            x2 = 0;                                          // змінна х2 - містить значення квадратного кореня №2
             
             if (D > 0)
             {
@@ -108,15 +115,12 @@ namespace ConsoleApp
             else if (D < 0)
             {
                 Console.WriteLine($"Дискримінант має від'ємне значення {D}, квадратні корені обчислити неможливо");
-                // x1 и x2 залишаються у значенні 0. Яке значення вказати? Чи вийти через return?
-                return;
+                return;                     // x1 и x2 обчислити неможливо. Виходжу через з циклу через return
             }
             
-            Console.WriteLine($"Значення змінної D: {D}");           //виводжу значення D для перевірки
-            Console.WriteLine($"Значення змінної x1: {x1}");
-            Console.WriteLine($"Значення змінної x2: {x2}");
-        }
-        
-        
+            Console.WriteLine($"Значення змінної D: {D}");           //виводжу отримане значення дискримінанту
+            Console.WriteLine($"Значення змінної x1: {x1}");         //виводжу отримане значення x1
+            Console.WriteLine($"Значення змінної x2: {x2}");         //виводжу отримане значення x2
+        }    
     }
 }
