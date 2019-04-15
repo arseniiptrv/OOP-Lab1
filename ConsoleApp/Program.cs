88cde66cadd93978c2953094b452e3b150fa4414
Arsenii Petrov <arseniiptrv@gmail.com>
2019-04-15 23:05:12 +0300

    feature(all): добавил 7 методов для решения уравнений (№№1-7) из ООП ЛР№1. Почистил код в оформленных ранее методах.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,59 +1,79 @@
 ﻿using System;
 
 namespace ConsoleApp
-{
+{   
     internal class Program
-    {
+    {   
         static void Main(string[] args)
         {
             System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo) 
             System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
             customCulture.NumberFormat.NumberDecimalSeparator = ".";
             System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+
+         
+
+
+            // ООП. Лабораторна робота 2
+            // Завдання №1. Введення значень для всіх рівнянь (№№ 1-7):
             
-            //Console.OutputEncoding = Encoding.Unicode;
-            //Console.InputEncoding = Encoding.Unicode;
-           
-            
-            
-            
-            
-            
-            
-            // Some test task
-            /*int[] arr = new int[] {1, 3, 5, 7, 9};
-            arr[2] = 15;
-            arr[5] = 25;
-            foreach (int q in arr)
+            Console.WriteLine("Введите значение x: ");
+            var xInput = Console.ReadLine();                      // юзер вводит значение х на консоли 
+            double x;                                             // переменная для хранения значения х
+            while (!double.TryParse(xInput, out x))
             {
-                Console.WriteLine(q);
+                Console.WriteLine("Вы ввели некорректное значение x, попробуйте ещё раз: ");
+                xInput = Console.ReadLine();
+            }
+            
+            
+            Console.WriteLine("Введите значение y: ");
+            var yInput = Console.ReadLine();                      // юзер вводит значение y на консоли 
+            double y;                                             // переменная для хранения значения y 
+            while (!double.TryParse(yInput, out y))
+            {
+                Console.WriteLine("Вы ввели некорректное значение y, попробуйте ещё раз: ");
+                yInput = Console.ReadLine();
+            }
+            
+            
+            Console.WriteLine("Введите значение z: ");
+            var zInput = Console.ReadLine();                      // юзер вводит значение y на консоли 
+            double z;                                             // переменная для хранения значения y 
+            while (!double.TryParse(zInput, out z))
+            {
+                Console.WriteLine("Вы ввели некорректное значение z, попробуйте ещё раз: ");
+                zInput = Console.ReadLine();
             }
 
-            for (int e = 0; e < arr.Length; e++)
-            {
-                Console.WriteLine(arr[e]);
-            }*/
+            var equationOne = EquationOne(x, y, z);
+            var equationTwo = EquationTwo(x, y, z);
+            var equationThree = EquationThree(x, y, z);
+            var equationFour = EquationFour(x, y, z);
+            var equationFive = EquationFive(x, y, z);
+            var equationSix = EquationSix(x, y, z);
+            var equationSeven = EquationSeven(x, y, z);
 
+            Console.WriteLine($"Значение уравнения 1 равно {equationOne}");
+            Console.WriteLine($"Значение уравнения 2 равно {equationTwo}");
+            Console.WriteLine($"Значение уравнения 3 равно {equationThree}");
+            Console.WriteLine($"Значение уравнения 4 равно {equationFour}");
+            Console.WriteLine($"Значение уравнения 5 равно {equationFive}");
+            Console.WriteLine($"Значение уравнения 6 равно {equationSix}");
+            Console.WriteLine($"Значение уравнения 7 равно {equationSeven}");
 
-
-
-
-            //var val = 233;
-
-            Int32 val = new Int32();
-            val = 233;
-            Console.WriteLine(val);
             
             
             
             
             
-            // ООП. Лабораторна робота 2
+            
             // Завдання №2: Виклик методу discriminant для обчислення коренів квадратного рівняння
 
             // Оголошую змінні х1, х2 та D, у які буде передано значення квадратних коренів
             // та дискримінанта після виклику метода discriminant
-            int x1;
+            /*int x1;
             int x2;
             int D;
             
@@ -98,19 +118,93 @@ namespace ConsoleApp
             
             
             // Викликаю метод дискримінант
-            discriminant(a, b, c, out x1, out x2, out D);
-            
-            
-            
-            
-            
-            
+            discriminant(a, b, c, out x1, out x2, out D);*/
         }
 
-        
-       
 
-        static void discriminant(int a, int b, int c, out int x1, out int x2, out int D)
+        
+        
+        
+        
+        // метод для рівняння №1 з завдання №1: 
+        static double EquationOne(double x, double y, double z)
+        {
+            var yPower = 2 - z;
+            var forSin = Math.Pow(y, yPower);
+            var leftSide = (2 * Math.Cos(x * x) - 0.5) / (0.5 + Math.Sin(forSin));
+            var rightSide = (z * z) / (7 - z / 3);
+
+            return Math.Round(leftSide + rightSide, 3);
+        }
+        
+        
+        // метод для рівняння №2 з завдання №1: 
+        static double EquationTwo(double x, double y, double z)
+        {
+            var xyPower = Math.Pow(3 + (x - y), 1 / 3);
+            var leftSide = xyPower / (x * x + z * z * z + 4);
+            var rightSide = Math.Tan(z);
+            return Math.Round(leftSide - rightSide, 3);
+        }
+        
+        
+        // метод для рівняння №3 з завдання №1: 
+        static double EquationThree(double x, double y, double z)
+        {
+            var znamenatel = Math.Abs(z - 2 * x / (1 + x * x * y * y));
+            var xPower = Math.Pow(x, Math.Abs(y));  
+            var result = (1 + Math.Sin(x + y)) / znamenatel * xPower + Math.Tan(1 / z);
+            return Math.Round(result, 3);
+        }
+        
+        
+        // метод для рівняння №4 з завдання №1: 
+        static double EquationFour(double x, double y, double z)
+        {
+            var yPower = 1 + 2 * Math.Sin(z);
+            var xyPow = Math.Pow(Math.Cos(x) - Math.Sin(y), yPower);
+            var result = xyPow * (z + z * z / 2 + z * z * z / 3 + z * z * z * z / 4);
+            return Math.Round(result, 3);
+        }
+             
+        
+        // метод для рівняння №5 з завдання №1: 
+        static double EquationFive(double x, double y, double z)
+        {
+            var xAbs = Math.Abs(x);
+            var yPower = -Math.Sqrt(xAbs);
+            var leftSide = Math.Sin(Math.Pow(y, yPower)) * (z - x / y);
+            var rightSide = Math.Sin(Math.Cos(z));
+            return Math.Round(leftSide + rightSide, 3);
+        }        
+        
+        
+        // метод для рівняння №6 з завдання №1:
+        static double EquationSix(double x, double y, double z)
+        {
+            var leftSide = Math.Sqrt(10 * (Math.Pow(x, 1 / 3) + Math.Pow(x, y + 2)));
+            var rightSide = Math.Sin(z - Math.Abs(x + y));
+            return Math.Round(rightSide * leftSide, 3);
+        }
+        
+        
+        // метод для рівняння №7 з завдання №1:
+        static double EquationSeven(double x, double y, double z)
+        {
+            var rightSide = z / 4 * Math.Sin(y) * (4 * Math.Abs(z - x) - 5) / (z * (x - y) + z * z);
+            return Math.Round(5 * Math.Cos(x) - rightSide, 3);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // метод дискримінант, завдання №2:
+        /*static void discriminant(int a, int b, int c, out int x1, out int x2, out int D)
         {
             D = b * b - 4 * a * c;                           // змінна D - містить значення дискримінанту
             x1 = 0;                                          // змінна х1 - містить значення квадратного кореня №1
@@ -134,6 +228,6 @@ namespace ConsoleApp
             Console.WriteLine($"Значення змінної D: {D}");           //виводжу отримане значення дискримінанту
             Console.WriteLine($"Значення змінної x1: {x1}");         //виводжу отримане значення x1
             Console.WriteLine($"Значення змінної x2: {x2}");         //виводжу отримане значення x2
-        }    
+        }*/    
     }
 }
--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -8,66 +8,125 @@ namespace ConsoleApplication2
         {
             // ООП. Лабораторна робота 2
             // Завдання №3
-            int x = 0;                             // счетчик, используется для обращения к эл-ту массива с индексом x в цикле
+            /*int x = 0;                             // счетчик, используется для обращения к эл-ту массива с индексом x в цикле
             int a = 1;                             // будет хранить введенное значение, распарсенное в число
-            int[] array = new int[100];            // массив, в который буду сохранять введеные значения
+            int[] array = new int[50];            // массив, в который буду сохранять введеные значения            
             Console.WriteLine("Будь ласка, введіть цілі числа. Для закінчення введення введіть 0");
 
+    
             while (a != 0)
             {
-                var input = Console.ReadLine();
-                int.TryParse(input, out a);
+                string input = Console.ReadLine();
                 
-                while (!int.TryParse(input, out a))
+                if (int.TryParse(input, out a))
                 {
-                    Console.WriteLine("Ви ввели помилкове значення. Будь ласка, спробуйте ще раз: ");
-                    input = Console.ReadLine();
+                    array[x] = a;
+                    x++;
                 }
-
-                array[x] = a;
-                x++;
+                else
+                {
+                                // TryParse возвращает 0 если не удалось пр-е, поэтому возвращаем значение a (любое отличное от 0) 
+                    Console.WriteLine("Ви ввели помилкове значення. Будь ласка, спробуйте ще раз: ");
+                    a = 1;
+                }                       
             }
             
             int pairNumb = 0;                  // кількість парних чисел
             int oddNumb = 0;                   // кількість непарних чисел
             int posNumb = 0;                   // кількість додатних чисел
             int negNumb = 0;                   // кількість від'ємних чисел
+                     
+            Numbers(ref pairNumb, ref oddNumb, ref posNumb, ref negNumb, array);*/          // виклик методу Numbers
             
-            Numbers(ref pairNumb, ref oddNumb, ref posNumb, ref negNumb, array);          // виклик методу Numbers
+            
+            
+            
+            
+            // вызов метода Fact - факториал числа (через рекурсию, тест):
+            /*Console.Write("Для обчислення факторіалу введіть значення: ");
+            var factInput = Console.ReadLine();
+            int factValue;
+            while (!int.TryParse(factInput, out factValue))
+            {
+                Console.WriteLine("Ви ввели помилкове значення. Будь ласка, спробуйте ще раз: ");
+                factInput = Console.ReadLine();             
+            }
+
+            var factResult = Fact(factValue);
+            Console.WriteLine($"Значення факторіалу числа {factValue} дорівнює {factResult}");*/
+            
+            
+            
+            
+            // вызов метода Fib - Фибоначчи (через рекурсию, тест):
+            /*Console.Write("Для обчислення фібоначчі введіть номер послідовності: ");
+            var fibInput = Console.ReadLine();
+            int fibValue;
+            while (!int.TryParse(fibInput, out fibValue))
+            {
+                Console.WriteLine("Ви ввели помилкове значення. Будь ласка, спробуйте ще раз: ");
+                fibInput = Console.ReadLine();
+            }
+            
+            var fibResult = Fib(fibValue);
+            Console.WriteLine($"Значення фібоначчі числа {fibValue} дорівнює {fibResult}");*/
+            
+            double x1 = Math.Sin()
         }
 
+        
+        
         
         
         public static void Numbers(ref int pairNumb, ref int oddNumb, ref int posNumb, ref int negNumb, params int[] numbs)
         {
             foreach (int i in numbs)
             {
-                if (i > 0 && i%2 == 0)
+                int firstCheck = 0;     // переменная для сравнения с остатком от деления
+                if (i > 0)
                 {
-                    pairNumb += 1;
                     posNumb += 1;
-                }
-                else if (i > 0 && i%2 != 0)
+                    firstCheck == (i % 2 == 0) ? pairNumb += 1 : oddNumb += 1;                                 
+                }               
+                else
                 {
-                    oddNumb += 1;
-                    posNumb += 1;
-                }
-                else if (i < 0 && i%2 == 0)
-                {
-                    pairNumb += 1;
                     negNumb += 1;
-                }
-                else if (i < 0 && i%2 != 0)
-                {
-                    oddNumb += 1;
-                    negNumb += 1;
-                }
+                    firstCheck = (i % 2 == 0) ? pairNumb += 1 : oddNumb += 1;
+                }                                        
             }
             
             Console.WriteLine($"Кількість парних чисел: {pairNumb}");
             Console.WriteLine($"Кількість непарних чисел: {oddNumb}");
             Console.WriteLine($"Кількість додатних чисел: {posNumb}");
             Console.WriteLine($"Кількість від'ємних чисел: {negNumb}");
+        }
+
+
+        
+        
+        
+        
+        // метод Fact - факториал числа (через рекурсию, тест):
+        static int Fact(int z)
+        {
+            if (z == 0)
+                return 1;
+            if (z == 1)
+                return 1;
+            return z * Fact(z - 2);
+        }
+
+        
+        
+        // метод Fib - Фибоначчи (через рекурсию, тест):
+        static int Fib(int y)
+        {
+            if (y == 0)              
+                return 0;
+            if (y == 1)
+                return 1; 
+            
+            return Fib(y - 2) + Fib(y - 1);            
         }
     }
 }
